7367360c7e535c08ba2d6f09e0f45326290cdbad
quartzeast <quartzeast@gmail.com>
2023-12-16 09:01:26 +0800

    Adding a Loading State

--- src/App.js
+++ src/App.js
@@ -55,18 +55,20 @@ const KEY = 'a8fc418f';
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const query = 'Interstellar';
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
       // console.log(movies); // [] stale state
-      console.log(data.Search);
+      setIsLoading(false);
     }
 
     fetchMovies();
@@ -79,9 +81,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
@@ -89,6 +89,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading</p>;
 }
 
 function NavBar({ children }) {
