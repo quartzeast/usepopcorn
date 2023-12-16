164e98cd3076531ecab2dd5a6554c993b4cde812
quartzeast <quartzeast@gmail.com>
2023-12-16 08:50:05 +0800

    useEffect to the Rescue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const tempMovieData = [
   {
@@ -56,11 +56,11 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=Interstellar`)
-    .then(res => res.json())
-    .then(data => setMovies(data.Search));
-
-  setWatched([]);
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=Interstellar`)
+      .then(res => res.json())
+      .then(data => setMovies(data.Search));
+  }, []);
 
   return (
     <>
